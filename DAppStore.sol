84fd3b80eeea9e9d92228170e803d56b26f74e19
Andy Tudhope <tuddy0525@gmail.com>
2019-02-26 12:21:36 +0200

    Rewrite contract for clarity - v1 Draft

--- DAppStore.sol
+++ DAppStore.sol
@@ -1,146 +1,195 @@
-pragma solidity 0.4.23;
+pragma solidity >=0.4.22 <0.6.0;
 
 import './token/MiniMeTokenInterface.sol';
 
+
 contract DAppStore {
     
-    uint256 constant percentage_num = 1;
-    uint256 constant percentage_den = 100000;
-    // if num = 1, and den = 100000, num/den = 0.000001
-
     MiniMeTokenInterface SNT;
 
     constructor (MiniMeTokenInterface _SNT) public {
         SNT = _SNT;
     }
-
-    struct Vote {
-        uint256 NumberMinted;
-        bool positive;
-    }
-
-    struct Dapp {
+    
+    /*
+        Total SNT in circulation
+    */
+    uint256 total == 3470483788;
+    /* 
+        According to calculations here: https://beta.observablehq.com/@andytudhope/dapp-store-snt-curation-mechanism
+        an interesting choices for the ceiling are around 0.4, but this requires more research/modelling.
+    */
+    uint8 ceiling == 0.4;
+    uint256 max = total * (ceiling/100);
+    /*
+        Whether we need more than an id param to identify arbitrary data must still be discussed.
+    */
+    struct Data = {
         address developer;
-        bytes category;
-        bytes name;
         bytes32 id;
-        uint256 SNTBalance;
-        uint256 effectiveBalance;
-        Vote[] votes;
+        uint256 balance;
+        uint256 rate;
+        uint256 available;
+        uint256 v_minted;
+        uint256 v_cast;
+        uint256 e_balance;
+        uint256 received;
     }
-
-    Dapp[] public dapps;
+    
+    Data[] public dapps;
     mapping(bytes32 => uint) public id2index;
     
-
     event DAppCreated(bytes32 id, uint256 amount);
-    event IncreasedDAppStake(bytes32 id, uint256 amount);
-
-    // -- TEST
-    function createDApp(bytes _category, bytes _name, bytes32 _id, uint256 _amountToStake) public {
-        require(_amountToStake != 0);
+    event upvote(bytes32 id, uint256 amount, uint256 newEffectiveBalance);
+    event downvote(bytes32 id, uint256 amount, uint256 votes_cast, uint256 newEffectiveBalance);
+    event withdraw(bytes32 id, uint256 amount, uint256 newEffectiveBalance);
+    
+    /*
+        Anyone can create a DApp (i.e an arb piece of data this contract happens to care about)
+        and there is no need even to start off with a positive balance (i.e. you can stake 0).
+    */
+    function createDapp(bytes32 _id, uint256 _amount) public { 
         require(SNT.allowance(msg.sender, address(this)) >= _amountToStake);
         require(SNT.transferFrom(msg.sender, address(this), _amountToStake));
-
+        
         uint dappIdx = dapps.length;
         
         dapps.length++;
 
-        Dapp storage d = dapps[dappIdx];
+        Data storage d = dapps[dappIdx];
         d.developer = msg.sender;
-        d.category = _category;
-        d.name = _name;
         d.id = _id;
-        d.SNTBalance = _amountToStake;
+        d.balance = _amount;
+        d.rate = 1 - (d.balance/max);
+        d.available = d.balance * d.rate;
+        d.v_minted = d.available ** (1/d.rate);
+        d.v_cast = 0;
+        d.e_balance = d.balance - ((d.v_cast/(1/d.rate))*(d.available/d.v_minted));
+        d.received = 0;
 
         id2index[_id] = dappIdx;
 
-        emit DAppCreated(_id, _amountToStake);
-    }   
-    
-    // -- TEST
-    function numVotesToMint(Dapp storage d, uint256 _SNTBalance) internal view returns(uint256) {  
-        uint TOTAL_SNT = d.SNTBalance;
-        uint SNT_PROPORTION = TOTAL_SNT * percentage_num;
-        assert(SNT_PROPORTION / TOTAL_SNT == percentage_num);
-        SNT_PROPORTION /= percentage_den;
-
-        uint num_votes_to_mint_at_1 =  percentage_den / percentage_num;
-
-        if (_SNTBalance <= SNT_PROPORTION) {
-            return num_votes_to_mint_at_1; 
-        }
-
-        if (_SNTBalance > SNT_PROPORTION) {
-            uint current_interval_index = _SNTBalance / SNT_PROPORTION;         
-            return num_votes_to_mint_at_1 + ((current_interval_index * (((_SNTBalance/100) - 1) / d.effectiveBalance)) * num_votes_to_mint_at_1);
-        }
-    } 
-
-    // -- TEST
-    function costOfMinting(bytes32 _id, uint256 _SNT) public view returns(uint256) {
-        uint dappIdx = id2index[_id];
-        Dapp storage d = dapps[dappIdx];
-        require(d.id == _id);
-        return numVotesToMint(d, _SNT);
+        emit DAppCreated(_id, _amount);
     }
     
-    // -- TEST
-    function stake(bytes32 _id, uint256 _amountToStake) public {
+    /* 
+        For use in the UI to show how the effective balance changes as a result of your donation.
+        This does _not_ mean it uses the curve: funds donated in upvoting go directly
+        to the Dapp's balance in the store, not the developer's pocket. It's just that actual
+        ranking is done on e_balance, and this must still be recalculated, even for upvotes.
+    */
+    function upvoteEffect(uint256 _id, uint256 _amount) public returns(uint256 effect) { 
         uint dappIdx = id2index[_id];
         Dapp storage d = dapps[dappIdx];
-
         require(d.id == _id);
-
-        require(_amountToStake != 0);
-        require(SNT.allowance(msg.sender, address(this)) >= _amountToStake);
-        require(SNT.transferFrom(msg.sender, address(this), _amountToStake));
         
-        d.SNTBalance += _amountToStake;
-
-        emit IncreasedDAppStake(_id, _amountToStake);
+        uint mBalance = d.balance + _amount;
+        uint mRate = 1 - (mBalance/max);
+        uint mAvailable = mBalance * mRate;
+        uint mMinted = mAvailable ** (1/mRate);
+        uint mEBalance = mBalance - ((mMinted/(1/mRate))*(mAvailable/mMinted));
+        
+        return (mEBalance - d.e_balance);
     }
     
-    // -- TEST
-    function upvote(bytes32 _id, uint256 _amount) public {
+    /*
+        Upvoting sends SNT directly to the contract, not to the developer and this gets
+        added to their balance, no curve required.
+    */
+    function upvote(uint256 _id, uint256 _amount) public { 
+        require(_amount != 0);
+        require(SNT.allowance(msg.sender, address(this)) >= _amount);
+        require(SNT.transferFrom(msg.sender, address(this), _amount));
+        
         uint dappIdx = id2index[_id];
         Dapp storage d = dapps[dappIdx];
         require(d.id == _id);
-        require(_amount != 0);
-
-        uint256 dappvotes = numVotesToMint(d, _amount);
-        mint(d, dappvotes, true);
-
-        require(SNT.allowance(msg.sender, d.developer) >= _amount);
-        require(SNT.transferFrom(msg.sender, d.developer, _amount));
+        
+        d.balance += _amount;
+        d.rate = 1 - (d.balance/max);
+        d.available = d.balance * d.rate;
+        d.v_minted = d.available ** (1/d.rate);
+        d.v_cast = 0;
+        d.e_balance = d.balance - ((d.v_cast/(1/d.rate))*(d.available/d.v_minted));
+        d.received = 0;
+        
+        emit upvote(_id, _amount, d.e_balance);
     }
     
-    // -- TEST
-    function downVote(bytes32 _id, uint256 _amount) public {
+    /*
+        For use in the UI, along with a slider that allows you to pick the % effect
+        you want to have on a DApp's rankings before calculating the cost to you.
+    */
+    function downvoteCost(uint256 _id, uint8 _percent_down) public returns(uint256 cost) { 
+        require(1 < _percent_down < 99);
+        
         uint dappIdx = id2index[_id];
         Dapp storage d = dapps[dappIdx];
         require(d.id == _id);
+        
+        uint balance_down_by = (_percent_down * d.e_balance);
+        uint votes_required = (balance_down_by * d.v_minted * d.rate) / d.available;
+        return cost = (d.available / (d.v_minted - (d.v_cast + votes_required))) * (votes_required / _percent_down / 100)
+    }
+    
+    /*
+        Downvoting sends SNT back to the developer of the DApp, while lowering it's
+        effective balance in the Store.
+    */
+    function downvote(uint256 _id, uint8 _percent_down, uint256 _amount) public { 
+        require(1 < _percent_down < 99);
         require(_amount != 0);
-
-        uint dappvotes = numVotesToMint(d, _amount);
-        mint(d, dappvotes, false);
-
-        uint negative_votes_before = d.effectiveBalance;
-        uint negative_votes_now = d.effectiveBalance + dappvotes;
-        uint negative_percent = ((negative_votes_now - negative_votes_before) * 100 / negative_votes_now );
-        d.effectiveBalance -=   d.effectiveBalance * negative_percent / 100;
-
-        assert(d.effectiveBalance < negative_votes_before);
-
-        // TODO: what happens if effectiveValance reaches 0;
-
+         
+        uint dappIdx = id2index[_id];
+        Dapp storage d = dapps[dappIdx];
+        require(d.id == _id);
+        
+        uint balance_down_by = (_percent_down * d.e_balance);
+        uint votes_required = (balance_down_by * d.v_minted * d.rate) / d.available;
+        
+        d.v_cast += votes_required;
+        d.e_balance = d.balance - ((d.v_cast/(1/d.rate))*(d.available/d.v_minted));
+        d.received += _amount;
+        
+        /*  
+            TODO: Not sure if this implies users must grant allowance to the DApp store
+            when upvoting, and then for each individual DApp they want to downvote? Could
+            be an annoying UI feature if so. Is there a better way?
+            
+            Also, there could be slippage between calling downvoteCost and actuall downvoting.
+            How to handle that graciously?
+        */
         require(SNT.allowance(msg.sender, d.developer) >= _amount);
         require(SNT.transferFrom(msg.sender, d.developer, _amount));
+        
+        emit downvote(_id, _amount, votes_required, d.e_balance);
     }
     
-    // -- TEST
-    function mint(Dapp storage d, uint256 _amount, bool _positive) internal {
-        d.votes.push(Vote(_amount, _positive));
+    /*  
+        Developers can withdraw an amount not more than what was available of the
+        SNT they originally staked minus what they have already received back in downvotes.
+    */
+    function withdraw(uint256 _id, uint256 _amount) public { 
+        uint dappIdx = id2index[_id];
+        Dapp storage d = dapps[dappIdx];
+        
+        require(d.id == _id);
+        require(msg.sender == d.developer);
+        require(_amount <= (d.available - d.received));
+        
+        d.balance -= _amount;
+        d.rate = 1 - (d.balance/max);
+        d.available = d.balance * d.rate;
+        d.v_minted = d.available ** (1/d.rate);
+        d.e_balance = d.balance - ((d.v_cast/(1/d.rate))*(d.available/d.v_minted));
+        
+        /*  
+            TODO: Not sure how to actually send funds out of this contract and back
+            to developers when they wish to withdraw?
+        */
+        require(SNT.allowance(address(this), d.developer) >= _amount);
+        require(SNT.transferFrom(address(this), d.developer, _amount));
+        
+        emit withdraw(_id, _amount, d.e_balance);
     }
-    
 }
